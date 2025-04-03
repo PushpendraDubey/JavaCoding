7cd889f6e7de18a66abcf526a91bb97f38c05794
Pushpendra Dubey <pushpendradubey5944@gmail.com>
2025-04-04 01:06:40 +0530

    Count digits of a number

---
+++ Random_Questions/Count_digits.java
@@ -0,0 +1,23 @@
+public class Count_digits {
+    public static int countDigits(int n){
+        // Write your code here.
+        int count = 0;
+        int temp = n;
+        while(temp>0){
+            int rem = temp%10;
+            if(rem != 0){
+                int rem2 = n%rem;
+                if( rem2 == 0){
+                    count++;
+                }
+            }
+            temp = temp/10;
+        }
+        return count;
+    }
+    public static void main(String [] args){
+        int n = 660;
+        int result = countDigits(n);
+        System.out.println(result);
+    }
+}
